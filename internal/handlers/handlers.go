94036d38ebed8c8136d394a72ff2de36fe8f8941
Stefani <stefaniisaeva@yandex.ru>
2025-10-11 23:43:12 +0300

    firstcom

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,56 @@
 package handlers
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HandleMain(res http.ResponseWriter, req *http.Request) {
+	path := "index.html"
+
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+
+	http.ServeFile(res, req, path)
+}
+
+func UploadHandler(w http.ResponseWriter, req *http.Request) {
+	req.ParseMultipartForm(10 << 20)
+	fileName := "myFile"
+
+	file, handler, err := req.FormFile(fileName)
+	if err != nil {
+		http.Error(w, "ошибка при получении файла", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	root, err := os.OpenRoot("uploads")
+	if err != nil {
+		http.Error(w, "внутренняя ошибка", http.StatusInternalServerError)
+		return
+	}
+	defer root.Close()
+
+	data, err := os.ReadFile(handler.Filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := service.Convert(string(data))
+
+	newFileName := root.Name() + "/" + time.Now().UTC().String() + filepath.Ext(handler.Filename)
+
+	if err := os.WriteFile(newFileName, []byte(result), 0755); err != nil {
+		log.Fatal(err)
+		http.Error(w, "внутренняя ошибка", http.StatusInternalServerError)
+		return
+	}
+}
